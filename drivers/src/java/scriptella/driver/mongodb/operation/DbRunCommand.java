c23d859bc2a77ab26828b14b7a714eccc52d73e7
Judilson <judilsonjunior@gmail.com>
2017-06-19 17:23:22 -0300

    MongoDB include

---
+++ drivers/src/java/scriptella/driver/mongodb/operation/DbRunCommand.java
@@ -0,0 +1,30 @@
+package scriptella.driver.mongodb.operation;
+
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import scriptella.driver.mongodb.bridge.MongoBridge;
+
+import java.util.Arrays;
+
+/**
+ * @author Fyodor Kupolov
+ * @version 1.0
+ */
+public class DbRunCommand extends MongoOperation {
+
+    public DbRunCommand(DBObject command) {
+        super(null, Arrays.<Object>asList(command));
+    }
+
+    @Override
+    public void executeScript(MongoBridge mongoBridge) {
+        mongoBridge.runCommand((DBObject) getFirstArgumentAsBson());
+    }
+
+    @Override
+    public DBCursor executeQuery(MongoBridge mongoBridge) {
+        throw new UnsupportedOperationException("runCommand operation is not supported in queries");
+    }
+
+
+}
